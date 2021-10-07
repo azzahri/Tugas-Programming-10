adf851326f87cb752f9b9da0bd8ad79467a65a61
azzahri <azzahri007@gmail.com>
2021-10-07 18:00:21 +0700

    chalange 4

--- Problem Solving/Assets/Script/circlemovement.cs
+++ Problem Solving/Assets/Script/circlemovement.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class circlemovement : MonoBehaviour
 {
     private Rigidbody2D circleRigidbody;
+    public float speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
         // get komponen
         circleRigidbody = GetComponent<Rigidbody2D>();
-
-        // force
-        circleRigidbody.AddForce(Vector2.one * 5, ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector2.right * Input.GetAxis("Horizontal") * Time.deltaTime * speed);
+        transform.Translate(Vector2.up * Input.GetAxis("Vertical") * Time.deltaTime * speed);
     }
+
+
 }
