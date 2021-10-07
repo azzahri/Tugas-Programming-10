026fc36d9b8c817872cb52e90e7ccef5b1e61fda
azzahri <azzahri007@gmail.com>
2021-10-07 16:39:02 +0700

    chalange 2

---
+++ Problem Solving/Assets/Script/circlemovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class circlemovement : MonoBehaviour
+{
+    private Rigidbody2D circleRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get komponen
+        circleRigidbody = GetComponent<Rigidbody2D>();
+
+        // force
+        circleRigidbody.AddForce(Vector2.one * 5, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
